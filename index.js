a0d3965b5944164aec0164d52af39cd363fc28f4
Natasha Paczko <paczkonatasha@gmail.com>
2024-05-18 15:10:11 -0300

    guardar

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.use(express.static('public')); //para cargar los archivos estaticos
 app.set('view engine','pug'); 
 app.set('views', './views');
 
+app.get ('/', async (req , res) => {
+const response = await ( 'https://fakestoreapi.com/products' );
+const productos = await response.json();
+res.render("index", {productos : productos});
+
+});
+
+
+
+
 app.listen(3000,() => {
 console.log('servidor corriendo en el puerto 3000');
 });
+
