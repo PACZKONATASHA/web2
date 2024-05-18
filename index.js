9100aaf5868501040e0ead441b84194c22f57f7c
Natasha Paczko <paczkonatasha@gmail.com>
2024-05-18 19:22:01 -0300

    guardar

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require ('express');
+const pug = require("pug");
 const app = express ();
 
 app.use(express.static('public')); //para cargar los archivos estaticos 
 
 app.set('view engine','pug'); 
-app.set('views', './views');
+app.set('views', './vistas');
 
 app.get ('/', async (req , res) => {
-const response = await ( 'https://fakestoreapi.com/products' );
+const response = await ( 'https://fakestoreapi.com/products');
 const productos = await response.json();
+// aplicar descuento
+//traducir titulo y descripcion
 res.render("index", {productos : productos});
 
 });
